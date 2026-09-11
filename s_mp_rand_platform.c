d0c643ffb76db2ff7592e8f0834f671d25cf8159
craftablescience <me@craftablescience.info>
2026-09-11 03:17:09 -0700

    fix: msvc debug builds, but in code this time

--- s_mp_rand_platform.c
+++ s_mp_rand_platform.c
@@ -124,10 +124,14 @@ static mp_err s_read_urandom(void *p, size_t n)
 }
 #endif
 
+#ifndef _WIN32
 mp_err s_read_arc4random(void *p, size_t n);
+#endif
 mp_err s_read_wincsp(void *p, size_t n);
+#ifndef _WIN32
 mp_err s_read_getrandom(void *p, size_t n);
 mp_err s_read_urandom(void *p, size_t n);
+#endif
 
 /*
  * Note: libtommath relies on dead code elimination
@@ -151,10 +155,14 @@ mp_err s_read_urandom(void *p, size_t n);
 mp_err s_mp_rand_platform(void *p, size_t n)
 {
    mp_err err = MP_ERR;
+#ifndef _WIN32
    if ((err != MP_OKAY) && MP_HAS(S_READ_ARC4RANDOM)) err = s_read_arc4random(p, n);
+#endif
    if ((err != MP_OKAY) && MP_HAS(S_READ_WINCSP))     err = s_read_wincsp(p, n);
+#ifndef _WIN32
    if ((err != MP_OKAY) && MP_HAS(S_READ_GETRANDOM))  err = s_read_getrandom(p, n);
    if ((err != MP_OKAY) && MP_HAS(S_READ_URANDOM))    err = s_read_urandom(p, n);
+#endif
    return err;
 }
 
